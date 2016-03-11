e43b68b98996ba9181ba76212e916cfff3cc18e2
soldier0083 <soldier0083@gmail.com>
2016-03-11 13:47:55 +0800

    02

--- app/src/main/java/com/example/heaven/gittest02/MainActivity.java
+++ app/src/main/java/com/example/heaven/gittest02/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Test02
+        //This is Test02
     }
 
 }
